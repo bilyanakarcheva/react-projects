7eda69359cc5c604d5e7787cbe747828ee13ec8f
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-10-22 23:41:16 +0300

    Create initial Form setup

---
+++ Notes-app/src/components/CustomForm.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const CustomForm = ({ addNote }) => {
+    const [note, setNote] = useState("");
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault(); // Prevents the page from refreshing
+        addNote({
+            details: note,
+            id: Date.now()
+        });
+        setNote("")
+
+        console.log(e.target[0].value); // Logs the value of the input
+    }
+
+    return (
+        <form
+            className="notes-form"
+            onSubmit={handleFormSubmit}
+        >
+            <p>{note}</p>
+            <div className="wrapper">
+                <input
+                    type="text"
+                    id="note"
+                    className="input"
+                    value={note}
+                    onInput={(e) => setNote(e.target.value)}
+                    required // This shows the pop-up message to add something if your field is empty.
+                    autoFocus
+                    maxLength={100}
+                    placeholder="Add a note..."
+                />
+                {/* <label
+                    htmlFor="note"
+                    className="label"
+                >
+                    Add a note
+                </label> */}
+            </div>
+            <button
+                className="btn"
+                aria-label="Add Note"
+                type="submit"
+            >
+                Add
+            </button>
+        </form>
+    )
+}
+
+export default CustomForm
