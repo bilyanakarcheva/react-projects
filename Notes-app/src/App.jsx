ffc70129102420bf1a559b6b78f0f06beb01ac81
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-10-23 23:43:55 +0300

    Add list of notes logic in App.jsx

--- Notes-app/src/App.jsx
+++ Notes-app/src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import './App.css'
 // custom components
 import CustomForm from './components/CustomForm'
+import NotesList from './components/NotesList';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
   const addNote = (note) => {
+    setNotes(currNotes => [...currNotes, note]);
     console.log(note);
   }
 
@@ -14,6 +18,7 @@ function App() {
         <h1>My Notes List</h1>
       </header>
       <CustomForm addNote={addNote} />
+      {notes && <NotesList notes={notes} />}
     </div>
   )
 }
