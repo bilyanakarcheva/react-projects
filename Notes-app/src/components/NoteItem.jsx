2a182c264ba3556441cb272291d98104aada18c3
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-10-24 20:00:05 +0300

    Add isEditing logic in NoteItem component

--- Notes-app/src/components/NoteItem.jsx
+++ Notes-app/src/components/NoteItem.jsx
@@ -1,28 +1,64 @@
-const NoteItem = ({ note }) => {
-  const handleDelete = () => {
+import { useState } from 'react';
 
+const NoteItem = ({ note, deleteNote, updateNote }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedNote, setUpdatedNote] = useState(note);
+
+  const handleDelete = () => {
+    deleteNote(note.id);
+  }
+
+  const handleUpdate = (id, updatedNote) => {
+    updateNote(id, updatedNote);
+    setIsEditing(false)
   }
 
   return (
     <li
       style={{ display: 'flex', gap: '2em' }}
     >
-      <label
-        htmlFor={note.id}
-      >
-        <p>{note.details}</p>
-      </label>
-      <button
-        style={{ background: 'yellow', color: 'black' }}>
-        {/* onClick={} */}
-        Update
-      </button>
-      <button
-        style={{ background: 'red' }}
-        onClick={handleDelete}
-      >
-        Delete
-      </button>
+      {isEditing ? (
+        <div className='btn-group'>
+          <input
+            type='text'
+            id={note.id}
+            value={updatedNote.details}
+            onInput={(e) => setUpdatedNote({...updatedNote, details: e.target.value})}
+            maxLength={100}
+          />
+          <button
+            style={{ background: 'green' }}
+            onClick={() => handleUpdate(note.id, updatedNote)}
+          >
+          Save
+          </button>
+          <button
+            style={{ background: 'red' }}
+            onClick={() => setIsEditing(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      ) : (
+        <div className='btn-group'>
+          <label
+            htmlFor={note.id}
+          >
+            <p>{updatedNote.details}</p>
+          </label>
+          <button
+            style={{ background: 'yellow', color: 'black' }}
+            onClick={() => setIsEditing(true)}>
+            Update
+          </button>
+          <button
+            style={{ background: 'red' }}
+            onClick={handleDelete}
+          >
+            Delete
+          </button>
+        </div>
+      )}
     </li>
   )
 }
