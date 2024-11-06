a2e6c0eeee1501d777a8e1b51044a5243bd3e484
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-11-06 21:41:19 +0200

    Add Express.js server logic to App.jsx note functions

--- Notes-app/src/App.jsx
+++ Notes-app/src/App.jsx
@@ -1,27 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 // custom components
 import CustomForm from './components/CustomForm'
 import NotesList from './components/NotesList';
-import useLocalStorage from './hooks/useLocalStorage';
+import {
+  fetchNotesFromAPI,
+  addNoteAPI,
+  toggleNoteAPI,
+  deleteNoteAPI,
+  updateNoteAPI
+} from './components/apiHelper';
 
 function App() {
-  const [notes, setNotes] = useLocalStorage('notes-storage', []);
+  //const [notes, setNotes] = useLocalStorage('notes-storage', []); // Using custom hook to store notes in local storage
+  const [notes, setNotes] = useState([]); // Using state to store notes
 
-  const addNote = (note) => {
-    setNotes(currNotes => [...currNotes, note]);
-    console.log(note);
-  }
+  const addNote = async (note) => {
+    const newNote = await addNoteAPI(note);
+    console.log(newNote);
+    if (newNote) {
+      setNotes(currNotes => [...currNotes, note]); // Every time a note is added, the notes array is updated and the component re-renders
+    }
+  } 
 
-  const deleteNote = (id) => {
-    setNotes(currNotes => currNotes.filter(n => n.id !== id));
-  }
-
-  const updateNote = (id, updatedNote) => {
-    setNotes(currNotes => currNotes.map(note => 
-      note.id === id ? updatedNote : note
+  const toggleNote = async (id) => {
+    const toggledNote = await toggleNoteAPI(id);
+    setNotes(currNotes => currNotes.map(note =>
+      note.id === id ? toggledNote : note
     ));
   };
+
+  const deleteNote = async (id) => {
+    const isDeleted = await deleteNoteAPI(id);
+    if (isDeleted) {
+      setNotes(currNotes => currNotes.filter(n => n.id !== id));
+    }
+  }
+
+  const updateNote = async (id, updatedNote) => {
+    const updated = await updateNoteAPI(id, updatedNote);
+    if (updated) {
+      setNotes(currNotes => currNotes.map(note =>
+        note.id === id ? updatedNote : note
+      ));
+    }
+  };
+
+  const loadNotes = async () => {
+    const notesItems = await fetchNotesFromAPI();
+    console.log('Notes', notesItems);
+    setNotes(notesItems);
+    console.log('Notes', notes);
+  };
+
+  useEffect(() => {
+    loadNotes();
+  }, []); // Empty array means this effect will only run once
 
   return (
     <div className='container'>
@@ -31,6 +65,7 @@ function App() {
       <CustomForm addNote={addNote} />
       {notes && (
         <NotesList
+          toggleNote={toggleNote}
           notes={notes}
           deleteNote={deleteNote}
           updateNote={updateNote}
