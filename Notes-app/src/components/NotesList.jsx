f709dcc292a9afe61d1e03af87a2c0fde2c57ce0
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-11-21 21:55:37 +0200

    Move styling from NoteList component to CSS file

--- Notes-app/src/components/NotesList.jsx
+++ Notes-app/src/components/NotesList.jsx
@@ -2,8 +2,10 @@ import NoteItem from "./NoteItem"
 
 const NotesList = ({ notes, deleteNote, updateNote, toggleNote }) => {
     return (
-        <ul style={{ display: 'grid', gap: '1.5em', margin: '2em' }}>
-            {/* Notes List */}
+        <ul
+            className="notes-list"
+            // style={{ display: 'grid', gap: '1.5em', margin: '2em' }}
+            >
             {notes.sort((a, b) => b.id - a.id).map(note => (
                 <NoteItem
                     key={note.id}
