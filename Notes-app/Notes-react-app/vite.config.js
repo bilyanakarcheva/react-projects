a9d2b1a640bb81bdfae5987d6d663990cc228d86
Bilyana Karcheva <karcheva.bilyana@gmail.com>
2024-11-25 12:06:01 +0200

    Amend vite.config.js to be able to create a build in Vercel

--- Notes-app/Notes-react-app/vite.config.js
+++ Notes-app/Notes-react-app/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  root: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'
+    }
+  },
   plugins: [react()],
 })
